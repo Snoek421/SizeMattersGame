4d5455f8c8ef03193807ec2ab47d15a1c8d866d9
LuckyGooseP <goose.man110@gmail.com>
2022-12-11 15:37:01 -0500

    added battery object, changed score

--- SizeMattersGame/GameScenes/ActionScene.cs
+++ SizeMattersGame/GameScenes/ActionScene.cs
@@ -44,6 +44,8 @@ namespace SizeMattersGame.GameScenes
         //added elements
         private gameObject door;
         private gameObject button;
+        private gameObject battery;
+        public int batteries = 0;
 
         InteractableObjectCollision test;
 
@@ -123,7 +125,11 @@ namespace SizeMattersGame.GameScenes
             button = new gameObject(game, spriteBatch, objectTex, objPos, 5);
             components.Add(button);
 
-            test = new InteractableObjectCollision(game, player, door, button, this);
+            objPos = new Vector2(Shared.stage.X - 240, 200);
+            battery = new gameObject(game, spriteBatch, objectTex, objPos, 0);
+            components.Add(battery);
+
+            test = new InteractableObjectCollision(game, player, door, button, battery, this);
             components.Add(test);
 
         }
@@ -153,9 +159,12 @@ namespace SizeMattersGame.GameScenes
             stageCompleted = false;
             this.button.isActive = false;
             this.door.isActive = false;
+            this.battery.isActive = false;
+            this.battery.Visible = true;
             this.components.Add(player);
             this.components.Add(button);
             this.components.Add(door);
+            this.components.Add(battery);
             this.components.Add(test);
             this._collideables.Add(player);
             addBorders();
@@ -252,7 +261,7 @@ namespace SizeMattersGame.GameScenes
 
             if (scoreScreen)
             {
-                string scoreScreenMessage = $"Your score was: {player.Score} \nTimer: {secondTimer} x 200 = {secondTimer * 200}\nBatteries: 0 x 1500\n\nPress enter to go to the next level";
+                string scoreScreenMessage = $"Your score was: {player.Score} \nTimer: {secondTimer} x 200 = {secondTimer * 200}\nBatteries: {batteries} x 1500\n\nPress enter to go to the next level";
                 spriteBatch.DrawString(regularFont, scoreScreenMessage, centerScreen, Color.Maroon);
 
             }
