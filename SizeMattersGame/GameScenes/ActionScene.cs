4b38cc5086bc3980cb353cf2cd01245026e9134a
Snoek421 <snoek-c@hotmail.com>
2022-12-11 14:47:48 -0500

    fixed action scene

--- SizeMattersGame/GameScenes/ActionScene.cs
+++ SizeMattersGame/GameScenes/ActionScene.cs
@@ -20,6 +20,7 @@ namespace SizeMattersGame.GameScenes
         private Game1 g;
         private SpriteBatch spriteBatch;
 
+
         private Player player;
         private List<CollideableObject> _collideables;
         private List<CollideableObject> _borders;
@@ -29,6 +30,11 @@ namespace SizeMattersGame.GameScenes
         private List<gameObject> _level2Objects;
         private LevelManager levelManager;
         public int currentLevel = 1;
+
+        //stuff for score
+        private double _timer = 60;
+        private int secondTimer;
+        private int previousSecond;
         private SpriteFont regularFont, highlightFont;
         private Vector2 timerPosition = new Vector2(70, 70);
         private Vector2 scorePosition = new Vector2(Shared.stage.X - 300, 70);
@@ -64,7 +70,6 @@ namespace SizeMattersGame.GameScenes
             SpriteFont hilight = g.Content.Load<SpriteFont>("fonts/highlightFont");
             regularFont = regular;
             highlightFont = hilight;
-            messagePosition = new Vector2(Shared.stage.X / 2, Shared.stage.Y / 2);
 
             //player character
             Texture2D playerTex = game.Content.Load<Texture2D>("images/SizeSpriteSheet");
@@ -238,10 +243,12 @@ namespace SizeMattersGame.GameScenes
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            spriteBatch.Begin();
             foreach (var collideable in _collideables)
             {
                 collideable.Draw(gameTime);
             }
+            base.Draw(gameTime);
 
             if (scoreScreen)
             {
